10da8d50e7df7054216286e3dde925b45bac08b7
xenor <xenor@codegreenit.com>
2012-10-01 16:59:33 +0800

    bootstrap in!

--- server.js
+++ server.js
@@ -3,18 +3,35 @@ var sockeio = require('socket.io'),
     cons = require('consolidate'),
     http = require("http"),
     child_process = require('child_process'),
-    app = express();
+    app = express(),
+    bootstrap = require('bootstrap-stylus'),
+       stylus = require('stylus');
 
-app.engine('haml', cons.haml);
-app.set('view engine', 'haml');
-app.set('views', __dirname + '/views');
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(bootstrap());
+}
 
-app.get('/', function(req, res){
-  res.render('index');
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views');
+  app.set('view options', { layout: false });
+  app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+  }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 
-var io = sockeio.listen(http.createServer(app), { 'log level': 1 })
+app.get('/', function(req, res){ res.render('index') });
 
-http.createServer(app).listen("3030");
+
+//var io = sockeio.listen(http.createServer(app), { 'log level': 1 })
+
+app.listen("3030");
 console.log("running scissors on port 3030!");
